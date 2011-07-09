27da0f7bdc6b07386203fa51f57b47b853be27bc
Mariano Guerra <luismarianoguerra@gmail.com>
2011-07-09 22:05:19 +0200

    add parser to the build script

--- src/build.sh
+++ src/build.sh
@@ -2,4 +2,6 @@
 
 erlc -pa . qrly.erl qrly_html.erl qrly_xml.erl
 erlc qrly_lexer.xrl && erlc qrly_lexer.erl && rm qrly_lexer.erl
+erlc qrly_parser.xrl && erlc qrly_parser.erl && rm qrly_parser.erl
+
 mv *.beam ../ebin
